626a2c3d9d57acc717bc90c82fac32fa407b6fd1
Samson Blackburn <Samson@Samson-Business.localdomain>
2021-01-28 15:49:27 +1000

    Single Page Application framework written, React Router and extra functionality implemented, base functionality of react website completed.

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>This is a test title</h2>
+        <p>Paragraph testing goes here...</p>
+ 
+        <p>Many of life's failures are people who did not realize how close they were to success when they gave up.</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
