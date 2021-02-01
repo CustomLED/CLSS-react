af9f0d8494ff145d71c6502c950975836e7f2801
Nicholas Bauer <nicholasbauer89@gmail.com>
2021-02-01 19:50:11 +1000

    installed axios, setup config/api folder, change get post/s functions to call from api

--- src/Services/postServices.js
+++ src/Services/postServices.js
@@ -1,9 +1,10 @@
 // This page seeds all the dummy data which will be changed to rails api calls.
+import postAPI from '../Components/config/api'
 
-const posts = [
- {id: 1, name: "Install at some place", text: "lots of hard work to make this", created_at: "2021-01-11T01:33:50.019Z", updated_at: "2021-01-11T01:33:50.019Z", admin_id: 2},
- {id: 2, name: "Install at znnsgf place", text: "lots of hard work to make this", created_at: "2021-01-11T01:33:50.019Z", updated_at: "2021-01-11T01:33:50.019Z", admin_id: 2}
- ]
+// const posts = [
+//  {id: 1, name: "Install at some place", text: "lots of hard work to make this", created_at: "2021-01-11T01:33:50.019Z", updated_at: "2021-01-11T01:33:50.019Z", admin_id: 2},
+//  {id: 2, name: "Install at znnsgf place", text: "lots of hard work to make this", created_at: "2021-01-11T01:33:50.019Z", updated_at: "2021-01-11T01:33:50.019Z", admin_id: 2}
+//  ]
 
 
 function transformPost(post) {
@@ -17,12 +18,17 @@ function transformPost(post) {
 }
 
 export async function getPosts() {
-	return posts
+    const response = await postAPI.get('/api/posts')
+    console.log(response);
+	return response.data
 	// return Promise.resolve(jokes)
 }
 
 
 export async function getPost(id) {
-	const post = posts.find(post => post.id == id)
-	return post ? transformPost(post) : null
+    const response = await postAPI.get(`/api/posts/${id}`)
+    return response.data
+
+	// const post = posts.find(post => post.id == id)
+	// return post ? transformPost(post) : null
 }
