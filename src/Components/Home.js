6cc2d4a29c7df9ca8e78dc5d6f53772352797cc1
Jordan Bradley <jordanrhysbradley@gmail.com>
2021-02-03 22:49:51 +1000

    static elements in about page complete

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import { Link } from "react-router-dom";
  
 class Home extends Component {
@@ -6,7 +7,7 @@ class Home extends Component {
     return (
       <div id="home-table">
         <Link to="/about">
-        <img src="https://res.cloudinary.com/custom-led-screen-solutions/image/upload/v1612337234/CLSS/clss-logo_cppcam.png" id="home-img" />
+        <img src="https://res.cloudinary.com/custom-led-screen-solutions/image/upload/v1612337234/CLSS/clss-logo_cppcam.png" alt="CLSS-logo" id="home-img" />
         </Link>
       </div>
     );
