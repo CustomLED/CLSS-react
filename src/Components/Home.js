cd9b27dc10718d9c8b0949953b8cbc5a6b5ccdf3
Jordan Bradley <jordanrhysbradley@gmail.com>
2021-02-03 22:25:18 +1000

    added link to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
  
 class Home extends Component {
   render() {
     return (
       <div id="home-table">
+        <Link to="/about">
         <img src="https://res.cloudinary.com/custom-led-screen-solutions/image/upload/v1612337234/CLSS/clss-logo_cppcam.png" id="home-img" />
+        </Link>
       </div>
     );
   }
