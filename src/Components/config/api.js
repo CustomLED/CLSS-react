48920d4150550908e4c08411c78a27dae26e42ec
Jordan Bradley <jordanrhysbradley@gmail.com>
2021-02-08 17:35:41 +1000

    heroku deployment

--- src/Components/config/api.js
+++ src/Components/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const postAPI = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://clss-api.herokuapp.com',
 });
 
 postAPI.interceptors.request.use((req) => {
