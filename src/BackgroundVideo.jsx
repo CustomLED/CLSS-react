718e4bc484b18beccfa900cb957bfa19d5fc46f1
CustomLED <78005684+CustomLED@users.noreply.github.com>
2021-09-28 23:55:11 +1000

    Update BackgroundVideo.jsx

--- src/BackgroundVideo.jsx
+++ src/BackgroundVideo.jsx
@@ -18,7 +18,7 @@ export default class BackgroundVideo extends React.Component {
             }}
             >
                 <source
-                src="https://www.dropbox.com/s/20x0oc0u26otoqe/clss-background-fade-v.mp4?dl=0p4"
+                src="https://www.dropbox.com/s/20x0oc0u26otoqe/clss-background-fade-v.mp4??raw=1p4"
                 type="video/mp4" />
             </video>
         )
